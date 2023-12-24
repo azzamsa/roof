b40c6c88ba45fbd37caf008583929c4c2432063a
azzamsa <me@azzamsa.com>
2023-12-25 06:46:18 +0700

    fix: `surround` doesn't work in Neogit rebase editor
    I need to override Neogit mappings.
    At the same time, it is also a chance to use Colemak keys.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,8 +6,9 @@ local M = {
 
 function M.config()
     local icons = require("plugins.icons")
+    local neogit = require("neogit")
 
-    require("neogit").setup({
+    neogit.setup({
         disable_signs = false,
         -- disable_hint = true,
         disable_context_highlighting = false,
@@ -38,68 +39,24 @@ function M.config()
         integrations = {
             diffview = true,
         },
-        -- Setting any section to `false` will make the section not render at all
-        -- sections = {
-        --   untracked = {
-        --     folded = false,
-        --   },
-        --   unstaged = {
-        --     folded = false,
-        --   },
-        --   staged = {
-        --     folded = false,
-        --   },
-        --   stashes = {
-        --     folded = true,
-        --   },
-        --   unpulled = {
-        --     folded = true,
-        --   },
-        --   unmerged = {
-        --     folded = false,
-        --   },
-        --   recent = {
-        --     folded = true,
-        --   },
-        -- },
-        -- override/add mappings
-        -- mappings = {
-        --   -- modify status buffer mappings
-        --   status = {
-        --     ["q"] = "Close",
-        --     ["1"] = "Depth1",
-        --     ["2"] = "Depth2",
-        --     ["3"] = "Depth3",
-        --     ["4"] = "Depth4",
-        --     ["<tab>"] = "Toggle",
-        --     ["x"] = "Discard",
-        --     ["s"] = "Stage",
-        --     ["a"] = "StageUnstaged",
-        --     ["<c-s>"] = "StageAll",
-        --     ["u"] = "Unstage",
-        --     ["U"] = "UnstageStaged",
-        --     ["d"] = "DiffAtFile",
-        --     ["$"] = "CommandHistory",
-        --     ["<c-r>"] = "RefreshBuffer",
-        --     ["o"] = "GoToFile",
-        --     ["<enter>"] = "Toggle",
-        --     ["<c-v>"] = "VSplitOpen",
-        --     ["<c-x>"] = "SplitOpen",
-        --     ["<c-t>"] = "TabOpen",
-        --     ["?"] = "HelpPopup",
-        --     ["D"] = "DiffPopup",
-        --     ["p"] = "PullPopup",
-        --     ["r"] = "RebasePopup",
-        --     ["P"] = "PushPopup",
-        --     ["c"] = "CommitPopup",
-        --     ["L"] = "LogPopup",
-        --     ["Z"] = "StashPopup",
-        --     ["b"] = "BranchPopup",
-        --     -- ["<space>"] = "Stage",
-        --     -- Removes the default mapping of "s"
-        --     -- ["s"] = "",
-        --   },
-        -- },
+        mappings = {
+            rebase_editor = {
+                ["p"] = "Pick",
+                ["r"] = "Reword",
+                ["e"] = "Edit",
+                ["s"] = "Squash",
+                ["f"] = "Fixup",
+                ["x"] = "Execute",
+                ["d"] = "Drop",
+                ["b"] = "Break",
+                ["q"] = "Close",
+                ["<cr>"] = "OpenCommit",
+                ["<a-e>"] = "MoveUp",
+                ["<a-n>"] = "MoveDown",
+                ["<c-c><c-c>"] = "Submit",
+                ["<c-c><c-k>"] = "Abort",
+            },
+        },
     })
 end
 
