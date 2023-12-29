c18517c20b8f36aa917699e33ccf2d83fafc5c97
azzamsa <me@azzamsa.com>
2023-12-29 21:35:52 +0700

    feat: use `Tokyo Night` theme
    The moon variant is more readable than other.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,24 +1,16 @@
+-- Color scheme
 return {
     {
-        "rebelot/kanagawa.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        commit = "c19b9023842697ec92caf72cd3599f7dd7be4456",
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        commit = "f247ee700b569ed43f39320413a13ba9b0aef0db",
+        opts = {},
         config = function()
-            require("kanagawa").setup({
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none",
-                                -- waveBlue1 is too dark for me. Hard to distinguish
-                                bg_visual = "#2D4F67", -- waveBlue2, default: waveBlue1
-                            },
-                        },
-                    },
-                },
+            require("tokyonight").setup({
+                style = "moon",
             })
-            vim.cmd.colorscheme("kanagawa")
+            vim.cmd.colorscheme("tokyonight")
         end,
     },
 }
