0e4c1741053c61619e3dc24586e885abf16cf00a
azzamsa <me@azzamsa.com>
2024-06-06 14:56:12 +0700

    feat: support hurl

--- lua/plugins/data.lua
+++ lua/plugins/data.lua
@@ -25,4 +25,25 @@ return {
         event = "VeryLazy",
         ft = "typst",
     },
+    {
+        "jellydn/hurl.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        ft = "hurl",
+        opts = {
+            -- Default formatter
+            formatters = {
+                json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
+                html = {
+                    "prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
+                    "--parser",
+                    "html",
+                },
+            },
+        },
+        keys = {},
+    },
 }
