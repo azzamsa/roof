c0dc6acf01a32b4d7e4fe81c5828ea55d8b1e762
Azzam S.A <noreply@azzamsa.com>
2025-04-11 20:35:33 +0700

    style: better tokyonight dark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,23 @@
 -- Color scheme
 return {
     {
-        "miikanissi/modus-themes.nvim",
+        "folke/tokyonight.nvim",
+        tag = "stable",
         lazy = false,
         priority = 1000,
         opts = {},
         config = function()
-            require("modus-themes").setup({
-                variant = "default",
+            require("tokyonight").setup({
+                -- `night` has the most darker bg
+                style = "moon",
+                on_colors = function(colors)
+                    colors.bg = "#000000"
+                    colors.fg = "#ffffff"
+                    -- default color is not WCAG AAA compliant
+                    colors.comment = "#a9b1d6"
+                end,
             })
-            vim.cmd([[colorscheme modus_vivendi]])
+            vim.cmd.colorscheme("tokyonight")
         end,
     },
 }
