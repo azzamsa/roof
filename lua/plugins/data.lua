8db376cbe54c7d44a083cfedc87fe4c222bd4db1
Azzam S.A <noreply@azzamsa.com>
2025-04-11 20:35:17 +0700

    feat: better ledger mode

--- lua/plugins/data.lua
+++ lua/plugins/data.lua
@@ -39,4 +39,12 @@ return {
             },
         },
     },
+    {
+        "wllfaria/ledger.nvim",
+        -- tree sitter needs to be loaded before ledger.nvim loads
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("ledger").setup()
+        end,
+    },
 }
