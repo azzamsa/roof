1bb42038ed3048a13e7df677c5a23c7c4350fd4f
Azzam S.A <noreply@azzamsa.com>
2025-04-21 15:16:26 +0700

    style: use dracula theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,17 @@
 -- Color scheme
 return {
     {
-        "folke/tokyonight.nvim",
-        tag = "stable",
+        "Mofiqul/dracula.nvim",
         lazy = false,
         priority = 1000,
         opts = {},
         config = function()
-            require("tokyonight").setup({
-                -- `night` has the most darker bg
-                style = "moon",
-                on_colors = function(colors)
-                    colors.bg = "#000000"
-                    colors.fg = "#ffffff"
-                    -- default color is not WCAG AAA compliant
-                    colors.comment = "#a9b1d6"
-                end,
+            require("dracula").setup({
+                colors = {
+                    bg = "#000000",
+                },
             })
-            vim.cmd.colorscheme("tokyonight")
+            vim.cmd.colorscheme("dracula")
         end,
     },
 }
