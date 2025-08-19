c1784d76c6624dea86bc6d86cb2d9241ab5f6c3b
Azzam S.A <noreply@azzamsa.com>
2025-08-19 21:24:29 +0700

    style: less popular theme usually break on a lot of edge cases

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,34 @@
 -- Color scheme
 return {
     {
-        "miikanissi/modus-themes.nvim",
+        "folke/tokyonight.nvim",
+        tag = "stable",
         lazy = false,
         priority = 1000,
         opts = {},
         config = function()
-            require("modus-themes").setup({
-                variant = "tinted",
+            require("tokyonight").setup({
+                style = "moon",
+                -- Modus Vivendi Tinted + Catppuccin mocha
                 on_colors = function(colors)
-                    -- https://github.com/miikanissi/modus-themes.nvim/blob/master/lua/modus-themes/colors.lua#L348
-                    colors.bg_main = "#000000"
-                    colors.bg_dim = "#11111b"
-                    colors.bg_hl_line = "#1e1e2e"
-                end,
-                on_highlights = function(highlights, colors)
-                    highlights.WinBarNC = { bg = colors.bg_tab_current }
+                    -- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_moon.lua
+                    colors.bg = "#000000"
+                    colors.bg_dark = "#11111b"
+                    colors.bg_float = "#11111b"
+                    colors.bg_popup = "#11111b"
+                    colors.bg_sidebar = "#11111b"
+
+                    colors.fg = "#ffffff"
+                    colors.fg_float = "#ffffff" -- last filename in recentfile
+                    colors.fg_dark = "#cdd6f4"
+                    colors.fg_sidebar = "#cdd6f4"
+
+                    colors.dark3 = "#a6adc8" -- recentfile
+                    -- default color is not WCAG AAA compliant
+                    colors.comment = "#a6adc8"
                 end,
             })
-            vim.cmd([[colorscheme modus_vivendi]])
+            vim.cmd.colorscheme("tokyonight")
         end,
     },
 }
